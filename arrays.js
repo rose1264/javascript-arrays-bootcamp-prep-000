ea84d5853067ca302a089b878378e7c7a046e632
Shumei Huang <rose1264@gmail.com>
2017-10-16 23:36:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
 var addElementToBeginningOfArray = []
-addElementToBeginningOfArray.unshift('rose')
+addElementToBeginningOfArray.unshift('rose');
