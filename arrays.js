a8d8ded4e466bb362207d107d21abe7fc9577136
Shumei Huang <rose1264@gmail.com>
2017-10-17 00:08:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,10 @@
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+
 function addElementToBeginningOfArray(array, element){
   var newarray = [element,...array]
   return newarray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  return array = [element, ...array]
 }
